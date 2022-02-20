fba3dcb21d4b933aa8247852d98d76911310bbab
steven <steven_ko@paradise-soft.com.tw>
2022-02-20 16:43:30 +0800

    feat: add socket

--- src/client/pages/chatRoom/index.ts
+++ src/client/pages/chatRoom/index.ts
@@ -1,4 +1,12 @@
 import "./index.css";
+import { io } from 'socket.io-client'
 import { name } from "@/utils";
 
 console.log("client side chatroom page", name);
+
+
+// 1.建立連接 -> node server
+const clientIo = io()
+clientIo.on('join', msg => {
+  console.log(msg)
+})
--- src/index.ts
+++ src/index.ts
@@ -1,11 +1,24 @@
 import devServer from "./server/dev";
 import prodServer from "./server/prod";
 import express from "express";
+import { Server, Socket } from 'socket.io'
+import http from 'http'
 
 import { name } from "@/utils";
 
 const port = 3000;
 const app = express();
+const server = http.createServer(app)
+const io = new Server(server)
+
+
+// 監測連接
+io.on('connection', (socket) => {
+  // 發射 welcome訊息到 join頻道
+  socket.emit('join', 'welcome1')
+  socket.emit('join', 'welcome2')
+  socket.emit('join', 'welcome3')
+})
 
 // 執行npm run dev本地開發 or 執行npm run start部署後啟動線上伺服器
 if (process.env.NODE_ENV === "development") {
@@ -16,6 +29,6 @@ if (process.env.NODE_ENV === "development") {
 
 console.log("server side", name);
 
-app.listen(port, () => {
+server.listen(port, () => {
   console.log(`The application is running on port ${port}.`);
 });
