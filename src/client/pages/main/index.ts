7f2575bb6fabb936b84d33a1637e2c6004a22ecb
steven <steven_ko@paradise-soft.com.tw>
2022-02-20 17:12:03 +0800

    feat(main): add btn click to chatroom

--- src/client/pages/main/index.ts
+++ src/client/pages/main/index.ts
@@ -1,4 +1,12 @@
 import "./index.css";
-import { name } from "@/utils";
 
-console.log("client side main page", name);
+const nameInput = document.getElementById('nameInput') as HTMLInputElement
+const roomSelect = document.getElementById('roomSelect') as HTMLSelectElement
+const startBtn = document.getElementById('startBtn') as HTMLButtonElement
+
+ startBtn.addEventListener('click', () => {
+   const userName = nameInput.value
+   const roomName = roomSelect.value
+
+   location.href = `/chatRoom/chatRoom.html?user_name=${userName}&room_name=${roomName}`
+ })
